fe2ddd97c398b42fa487c13d86d576f70cf5e81b
Amil Silahic <jinobie02@gmail.com>
2023-11-09 13:37:05 -0700

    adding form-card component

---
+++ resources/views/components/form-card.blade.php
@@ -0,0 +1,3 @@
+<div class="flex mt-12 p-4 rounded-xl bg-gray-200">
+    {{$slot}}
+</div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,10 @@
 <x-layout>
     <h1 class="text-red-500">Hello World</h1>
 </x-layout>
+
+<x-form-card>
+    <form action="">
+        <label for="name">Name</label>
+        <input type="text">
+    </form>
+</x-form-card>
