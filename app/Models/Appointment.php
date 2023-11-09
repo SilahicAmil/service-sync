9a55fd7e76ee7c1596c09a890cc740f75e196a06
Amil Silahic <jinobie02@gmail.com>
2023-11-09 13:03:01 -0700

    adding more fillable items

--- app/Models/Appointment.php
+++ app/Models/Appointment.php
@@ -8,7 +8,20 @@ use Illuminate\Database\Eloquent\Model;
 class Appointment extends Model
 {
     use HasFactory;
-    
+
+    protected $fillable = [
+        'car_make',
+        'car_model',
+        'car_year',
+        'car_miles',
+        'vehicle_vin',
+        'service_name',
+        'service_date',
+        'service_price',
+        'additional_notes',
+        'user_id',
+    ];
+
     public function user(): \Illuminate\Database\Eloquent\Relations\BelongsTo
     {
         return $this->belongsTo(User::class, 'user_id');
