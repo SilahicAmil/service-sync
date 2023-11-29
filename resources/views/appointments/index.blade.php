14a8e513007f330a74497f373a337f04387e5873
Amil Silahic <jinobie02@gmail.com>
2023-11-29 12:31:09 -0700

    moving the if checks to the /appointments route

--- resources/views/appointments/index.blade.php
+++ resources/views/appointments/index.blade.php
@@ -1,1 +1,14 @@
-<h1>Hello</h1>
+<x-layout>
+    <h2>User Appointments:</h2>
+    <ul class="mb-4">
+        @foreach($appointments as $apt)
+            {{--            change this to use auth user id eventually--}}
+            @if($apt->user_id = 2)
+                <x-form-card>
+                    <li>{{$apt->service_name}} - {{$apt->user->name}}</li>
+                </x-form-card>
+            @endif
+        @endforeach
+    </ul>
+
+</x-layout>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,15 +5,6 @@
         <h1>Total Appointments: &nbsp</h1>
             <p>{{$appointments->count()}}</p>
     </x-form-card>
-    <h2>User Appointments:</h2>
-    <ul>
-        @foreach($appointments as $apt)
-{{--            change this to use auth user id eventually--}}
-            @if($apt->user_id = 2)
-                <li>{{$apt->service_name}} - {{$apt->user->name}}</li>
-            @endif
-        @endforeach
-    </ul>
 
 </x-layout>
 
