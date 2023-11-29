ab77e76eea846d83f91ff4bb2453f87ad3de50ba
Amil Silahic <jinobie02@gmail.com>
2023-11-29 12:28:54 -0700

    adding some basic if checks on main page to only show certain appointmentsd

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,11 +1,19 @@
 <x-layout>
 
     <x-form-card>
-        <a href="/appointments">Wahat</a>
+        <a href="/appointments">View All &nbsp</a>
         <h1>Total Appointments: &nbsp</h1>
             <p>{{$appointments->count()}}</p>
-
     </x-form-card>
+    <h2>User Appointments:</h2>
+    <ul>
+        @foreach($appointments as $apt)
+{{--            change this to use auth user id eventually--}}
+            @if($apt->user_id = 2)
+                <li>{{$apt->service_name}} - {{$apt->user->name}}</li>
+            @endif
+        @endforeach
+    </ul>
 
 </x-layout>
 
