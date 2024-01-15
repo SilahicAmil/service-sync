27ad3d158d47baaad3d10bc559117fe35f661d5b
Amil Silahic <jinobie02@gmail.com>
2024-01-15 11:51:15 -0700

    redoing how the errors display for creating service

--- resources/views/appointments/create.blade.php
+++ resources/views/appointments/create.blade.php
@@ -36,20 +36,39 @@ natcasesort($service_names);
             </select>
             <label for="vehicle_make">Vehicle Make</label>
             <label>
-                <input type="text" name="vehicle_make">
+                <input type="text" name="vehicle_make" value="{{old('vehicle_make')}}">
             </label>
+
+            @error('vehicle_make')
+            <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+            @enderror
+
             <label for="vehicle_model">Vehicle Model</label>
             <label>
-                <input type="text" name="vehicle_model">
+                <input type="text" name="vehicle_model" value="{{old('vehicle_model')}}">
             </label>
+
+            @error('vehicle_model')
+            <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+            @enderror
+
             <label for="vehicle_year">Vehicle Year</label>
             <label>
-                <input type="number" name="vehicle_year">
+                <input type="number" name="vehicle_year" value="{{old('vehicle_year')}}">
             </label>
+
+            @error('vehicle_year')
+            <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+            @enderror
+
             <label for="vehicle_miles">Vehicle Miles</label>
             <label>
-                <input type="number" name="vehicle_miles">
+                <input type="number" name="vehicle_miles" value="{{old('vehicle_miles')}}">
             </label>
+
+            @error('vehicle_miles')
+            <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+            @enderror
 
             <label for="service_name">Service</label>
             <label>
@@ -61,27 +80,32 @@ natcasesort($service_names);
             </label>
             <label for="service_date">Service Date</label>
             <label>
-                <input type="date" name="service_date">
+                <input type="date" name="service_date" value="{{old('service_date')}}">
             </label>
+
+            @error('service_date')
+            <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+            @enderror
+
             <label for="vehicle_vin">Vehicle Vin</label>
             <label>
-                <input type="text" name="vehicle_vin">
+                <input type="text" name="vehicle_vin" value="{{old('vehicle_vin')}}">
             </label>
+
+            @error('vehicle_vin')
+            <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+            @enderror
+
             <label for="additional_notes">Notes</label>
             <label>
-                <input type="text" name="additional_notes">
+                <input type="text" name="additional_notes" value="{{old('additional_notes')}}">
             </label>
+
+            @error('additional_notes')
+            <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+            @enderror
+
             <button type="submit">Submit</button>
         </form>
-        @if ($errors->any())
-            <div class="alert alert-danger">
-                <ul>
-                    @foreach ($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-            </div>
-        @endif
-
     </x-form-card>
 </x-layout>
